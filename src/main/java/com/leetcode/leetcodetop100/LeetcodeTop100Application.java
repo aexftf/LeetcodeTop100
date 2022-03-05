6d92980d021cbc36895febfdd3cd5157488bd9cd
jacksun <446729313@qq.com>
2022-03-05 08:52:38 +0800

    Initial commit

---
+++ src/main/java/com/leetcode/leetcodetop100/LeetcodeTop100Application.java
@@ -0,0 +1,13 @@
+package com.leetcode.leetcodetop100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeTop100Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeTop100Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/leetcode/leetcodetop100/entity/code1.java
@@ -0,0 +1,16 @@
+package com.leetcode.leetcodetop100.entity;
+
+/**
+ * @author jacksun
+ */
+public class code1 {
+
+
+
+
+
+
+
+
+}
+
