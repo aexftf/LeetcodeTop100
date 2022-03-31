b3d1beeb26bf2d5f0272b90075619fce7ac63718
partner <446729313@qq.com>
2022-03-31 10:46:49 +0800

    1

--- src/test/java/com/leetcode/leetcodetop100/LeetcodeTop100ApplicationTests.java
+++ src/test/java/com/leetcode/leetcodetop100/LeetcodeTop100ApplicationTests.java
@@ -3,11 +3,52 @@ package com.leetcode.leetcodetop100;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+
 @SpringBootTest
 class LeetcodeTop100ApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+//    void contextLoads() {
+
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+
+
+            Set<List<Integer>> lists = new HashSet<>();
+
+
+            for (int i = 0; i < nums.length; i++) {
+
+                int left = i + 1, right = nums.length - 1;
+
+                while (left < right) {
+                    if (nums[left] + nums[right] + nums[i] == 0) {
+
+                        lists.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                        left++;
+                        right--;
+                    } else if (nums[i] + nums[left] + nums[right] < 0) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            List<List<Integer>> list = new ArrayList<>(lists);
+            return list;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
